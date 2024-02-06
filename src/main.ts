de273aeb006cad608f90b0c39c372d48c64dbaf9
Nika Natroshvili <nikanatroshvili7@gmail.com>
2024-02-06 23:13:04 +0400

    feat: enable cors from anywhere so it will be easier to connect from frontend while in development state

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: '*',
+    exposedHeaders: '*',
+    methods: '*',
+  });
   await app.listen(45184);
 }
 bootstrap();
